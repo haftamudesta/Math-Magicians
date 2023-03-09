e4e6f50cd880c1680bc1a5e65018b0052e987dd2
haftamudesta <desta451616@hotmail.com>
2023-03-09 15:07:01 +0300

    Modify test

--- src/__tests__/operate.test.jsx
+++ src/__tests__/operate.test.jsx
@@ -1,26 +1,33 @@
+import { render, screen } from '@testing-library/react';
 import operate from '../logic/operate';
-
-const a = 200;
-const b = 50;
+import Calculator from '../component/Calculator';
 
 describe('Check if operate function gives the results', () => {
+  const obj = {
+    a: 200,
+    b: 50,
+  };
   test('Test multiply a * b to get 10,000', () => {
-    expect(operate(a, b, 'x')).toBe('10000');
+    expect(operate(obj.a, obj.b, 'x')).toBe('10000');
   });
 
   test('Test subtract a - b to get 150', () => {
-    expect(operate(a, b, '-')).toBe('150');
+    expect(operate(obj.a, obj.b, '-')).toBe('150');
   });
 
   test('Test Add a + b to get 250', () => {
-    expect(operate(a, b, '+')).toBe('250');
+    expect(operate(obj.a, obj.b, '+')).toBe('250');
   });
 
   test('Test divide a ÷ b to get 4', () => {
-    expect(operate(a, b, '÷')).toEqual('4');
+    expect(operate(obj.a, obj.b, '÷')).toEqual('4');
   });
 
   test('Test remainder a % b to get 0', () => {
-    expect(operate(a, b, '%')).toEqual('0');
+    expect(operate(obj.a, obj.b, '%')).toEqual('0');
   });
 });
+
+render(<Calculator />);
+const calculatorElement = screen.getByText('AC');
+expect(calculatorElement).toBeInTheDocument();
