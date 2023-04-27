5ea0a4001ef438b67113ed565f1b18da4e6c51b2
haftamudesta <desta451616@hotmail.com>
2023-04-28 00:37:50 +0300

    Modify hooks.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import Calculator from './component/Calculator';
+import Quetes from './component/Quetes';
 
 function App() {
   return (
     <div>
       <h1 className="title">Caculator with react</h1>
       <Calculator />
+      <Quetes />
     </div>
   );
 }
